ff64ebbe20a2ac3ebda16065ef62d889d9b7bae2
nikipdsci <124737571+nikipdsci@users.noreply.github.com>
2024-08-02 08:52:27 -0700

    added the missing signup in scripts.js

--- Code/public/scripts.js
+++ Code/public/scripts.js
@@ -37,31 +37,6 @@ async function checkDbConnection() {
         });
 }
 
-// Fetches data from the demotable and displays it.
-// async function fetchAndDisplayUsers() {
-//     const tableElement = document.getElementById('demotable');
-//     const tableBody = tableElement.querySelector('tbody');
-//
-//     const response = await fetch('/demotable', {
-//         method: 'GET'
-//     });
-//
-//     const responseData = await response.json();
-//     const demotableContent = responseData.data;
-//
-//     // Always clear old, already fetched data before new fetching process.
-//     if (tableBody) {
-//         tableBody.innerHTML = '';
-//     }
-//
-//     demotableContent.forEach(user => {
-//         const row = tableBody.insertRow();
-//         user.forEach((field, index) => {
-//             const cell = row.insertCell(index);
-//             cell.textContent = field;
-//         });
-//     });
-// }
 
 // This function resets or initializes the demotable.
 async function resetDemotable() {
@@ -233,26 +208,48 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+async function signUp(event) {
+    event.preventDefault();
+
+    const city = document.getElementById('signUpC').value;
+    const provinceState = document.getElementById('signUpPr').value;
+    const username = document.getElementById('signUpU').value;
+    const password = document.getElementById('signUpP').value;
+    const phoneNo = document.getElementById('signUpPn').value;
+    const email = document.getElementById('signUpE').value;
+    const name = document.getElementById('signUpN').value;
+
+
+    const response = await fetch('/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            City: city,
+            ProvinceState:provinceState,
+            Username: username,
+            Password: password,
+            PhoneNo: phoneNo,
+            Email: email,
+            Name: name
+        })
+    });
+
+    const responseData = await response.json();
+
+    const messageElement = document.getElementById('insertResultMsgSU');
+
+    if (responseData.success) {
+        messageElement.textContent = "Signed up successfully! You can log in now";
+    } else {
+        messageElement.textContent = "Error signing up!" +
+            "Either Email or Phone Number or Email is already in Use";
+    }
+}
+
 window.onload = function() {
     checkDbConnection();
     document.getElementById("loginform").addEventListener("submit", login);
+    document.getElementById("signUpform").addEventListener("submit", signUp);
 };
-
-// ---------------------------------------------------------------
-// Initializes the webpage functionalities.
-// Add or remove event listeners based on the desired functionalities.
-// window.onload = function() {
-//     checkDbConnection();
-//     // fetchTableData();
-//     // document.getElementById("resetDemotable").addEventListener("click", resetDemotable);
-//     // document.getElementById("insertDemotable").addEventListener("submit", insertDemotable);
-//     // document.getElementById("updataNameDemotable").addEventListener("submit", updateNameDemotable);
-//     // document.getElementById("countDemotable").addEventListener("click", countDemotable);
-//     document.getElementById("loginform").addEventListener("submit",login);
-// };
-
-// General function to refresh the displayed table data.
-// You can invoke this after any table-modifying operation to keep consistency.
-// function fetchTableData() {
-//     fetchAndDisplayUsers();
-// }
